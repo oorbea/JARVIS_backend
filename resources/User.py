ba8db6fc0c6e36a49351b0c7c8109b57576d98ab
Oriol Orbea <oriol.orbea@gmail.com>
2025-10-08 19:45:51 +0200

    user delete

--- resources/User.py
+++ resources/User.py
@@ -2,7 +2,7 @@ import traceback
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_smorest import Blueprint, abort
 from flask.views import MethodView
-from flask import current_app as app, jsonify
+from flask import Response, current_app as app, jsonify
 from db import db
 from models.Person import Person
 from models.User import User
@@ -140,6 +140,25 @@ class UserEndpoint(MethodView):
             db.session.rollback()
             abort(500, message=str(e))
 
+    @jwt_required()
+    @blp.response(204, description="User successfully deleted.")
+    @blp.response(401, description="Missing or invalid JWT.")
+    @blp.response(404, description="User not found.")
+    @blp.response(500, description="Internal Server Error")
+    def delete(self):
+        """Delete my user account."""
+        try:
+            email:str = get_jwt_identity()
+            user:User|None = User.query.get(email)
+            if not user:
+                abort(404, message="User not found.")
+            db.session.delete(user)
+            db.session.commit()
+            return Response(status=204)
+        except Exception as e:
+            traceback.print_exc()
+            db.session.rollback()
+            abort(500, message=str(e))
 
 @blp.route('/login')
 class LoginEndpoint(MethodView):

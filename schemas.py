cca724f5e2f06c47c8d065962e72f3aaee53dea2
Oriol Orbea <oriol.orbea@gmail.com>
2025-10-08 18:34:06 +0200

    password verification

--- schemas.py
+++ schemas.py
@@ -1,9 +1,22 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 from enums.CourtesyTitle import CourtesyTitle
+
+class Password(fields.String):
+    def __init__(self, *args, **kwargs):
+        validators = [
+            validate.Length(min=8, error="The password must be at least 8 characters long."),
+            validate.Regexp(
+            r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$',
+            error="The password must contain at least one lowercase letter, one uppercase letter, and one number."
+            )
+        ]
+        kwargs.setdefault('validate', validators)
+        kwargs.setdefault('load_only', True)
+        super().__init__(*args, **kwargs)
 
 class UserRegisterSchema(Schema):
     email = fields.Email(required=True, metadata={"description": "The user's email address."})
-    password = fields.String(required=True, load_only=True, metadata={"description": "The user's password."})
+    password = Password(required=True, metadata={"description": "The user's password."})
     name = fields.String(required=True, metadata={"description": "The user's first name."})
     surname = fields.String(required=True, metadata={"description": "The user's surname."})
     courtesy_title = fields.Enum(CourtesyTitle, required=True, metadata={"description": "The user's courtesy title."})
